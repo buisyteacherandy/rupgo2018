316211c79f7a13e302b7fa989ae73b4846e69445
Yonga <jorgeggayon@gmail.com>
2018-05-26 20:43:58 -0500

    Confing new server for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 
 set :stage, :production
 
-server '18.222.63.160', user: 'ubuntu', roles: %w{app web db}
+server 'ec2-18-222-63-160.us-east-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{app web db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
