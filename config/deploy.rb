d7d8018c5b62936ce77448841fd3001d60f85721
Yonga <jorgeggayon@gmail.com>
2018-05-26 22:13:46 -0500

    Confing new server for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,107 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :branch, :master
+set :tmp_dir, "/home/deploy/tmp"
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :copy_exclude, [".git", "spec"]
+
+namespace :deploy do
+
+ desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+ desc 'DB Seeds'
+  task :seed do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+ desc 'DB Purge'
+  task :purge do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:purge'
+        end
+      end
+    end
+  end
+
+ desc 'DB Migrate'
+  task :migrate do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:migrate'
+        end
+      end
+    end
+  end
+
+ desc 'DB Reset'
+  task :reset do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:reset DATABASE DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
+        end
+      end
+    end
+  end
+
+ task :logs, :file do |t, args|
+    if args[:file]
+      on roles(:app) do
+        execute "tail -f #{shared_path}/log/#{args[:file]}.log"
+      end
+    else
+      puts "please specify a logfile e.g: 'rake logs:tail[logfile]"
+      puts "will tail 'shared_path/log/logfile.log'"
+      puts "remember if you use zsh you'll need to format it as:"
+      puts "rake 'logs:tail[logfile]' (single quotes)"
+    end
+  end
+
+ after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+set :default_env, { "SECRET_KEY_BASE" => "XXXX", "RUPGO_DATABASE_PASSWORD" => "rupgo_password" }
